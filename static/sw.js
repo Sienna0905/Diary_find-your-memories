a34ab5247c572dcf4838f19f96f51f30f17d209a
Sienna0905 <csee3663@gmail.com>
2026-04-22 19:08:46 +0900

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'fym-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const c = r.clone();
+      caches.open(CACHE).then(cache => cache.put(e.request, c));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
